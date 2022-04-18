2887e90f573ebc2c5b5830b0fb1fc547afac41d7
Wiqiro <williamnoimbert@utbm.fr>
2022-04-18 21:46:18 +0200

    Format again

--- board.h
+++ board.h
@@ -1,52 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef enum
-{
+
+typedef enum{
     TRUE = 1,
     FALSE = 0,
 } Bool;
 
-typedef struct Piece
-{
+
+typedef struct Piece {
     Bool color;
     int type;
 } Piece;
 
 /**
  * @brief Create size*size checkers board
- *
+ * 
  * @param size Number of culumns and rows of the board
- * @return Piece**
+ * @return Piece** 
  */
-Piece **CreateAndInitialiazeBoard(int size);
+Piece** CreateAndInitialiazeBoard(int size);
 
 /**
  * @brief Place the pawns in the board
- *
+ * 
  * @param board Board that is modified by the function
  * @param size Size of the board
  */
-void InitializeBoard(Piece **board, int size);
+void InitializeBoard(Piece** board, int size);
 
 /**
  * @brief Print the two letters corresponding to a piece
- *
+ * 
  * @param piece Piece that is going to be print by the function
  */
 void PrintPiece(Piece piece);
 
 /**
  * @brief Print a checkers board
- *
+ * 
  * @param board Board that is going to be print by the function
  * @param size Size of the board
  */
-void PrintBoard(Piece **board, int size);
+void PrintBoard(Piece** board, int size);
 
 /**
  * @brief Check if a move is legal
- *
+ * 
  * @param board Board where the move has to be check
  * @param size Size of the board
  * @param startcol Column of the actual position of the piece
@@ -55,11 +55,11 @@ void PrintBoard(Piece **board, int size);
  * @param targrow Row of the targetted position of the piece
  * @return Bool : 0 if the move is illegal, 1 if the move is legal
  */
-Bool MoveCheck(Piece **board, int size, int startcol, int startrow, int targcol, int targrow);
+Bool MoveCheck(Piece** board, int size, int startcol, int startrow, int targcol, int targrow);
 
 /**
  * @brief Execute the specified move (doesn't check if the move is possible)
- *
+ * 
  * @param board Board where the move has to be executed
  * @param startcol Column of the actual position of the piece
  * @param startrow Row of the actual position of the piece
@@ -67,4 +67,4 @@ Bool MoveCheck(Piece **board, int size, int startcol, int startrow, int targcol,
  * @param targrow Row of the targetted position of the piece
  * @return int : number of captured pieces
  */
-int MoveExecution(Piece **board, int startcol, int startrow, int targcol, int targrow);
+int MoveExecution(Piece** board, int startcol, int startrow, int targcol, int targrow);
--- main.c
+++ main.c
@@ -3,83 +3,78 @@
 #include <ctype.h>
 #include <board.h>
 
-/*
 
-8 |     PB    PB    PB    PB
-7 |  PB    PB    PB    PB
-6 |     PB    PB    PB    PB
-5 |
-4 |
-3 |  PN    PN    PN    PN
-2 |     PN    PN    PN    PN
-1 |  PN    PN    PN    PN
+/* 
+
+8 |     PB    PB    PB    PB 
+7 |  PB    PB    PB    PB    
+6 |     PB    PB    PB    PB 
+5 |                          
+4 |                          
+3 |  PN    PN    PN    PN    
+2 |     PN    PN    PN    PN 
+1 |  PN    PN    PN    PN    
      __ __ __ __ __ __ __ __
-     a  b  c  d  e  f  g  h
+     a  b  c  d  e  f  g  h 
 
 */
 
-int main(int argc, char *argv[])
-{
-    int size = 8, moves = 0;
-    Piece **CheckersBoard = CreateAndInitialiazeBoard(size);
-    Bool turn;
-    int startcol, startrow, targcol, targrow, whitepieces, blackpieces;
 
-    PrintBoard(CheckersBoard, size);
+int main(int argc, char* argv[]) {
+    int size=8, moves = 0;
+    Piece** CheckersBoard = CreateAndInitialiazeBoard(size);
+    Bool turn;
+    int startcol, startrow, targcol, targrow, whitepieces , blackpieces;
+
+    PrintBoard(CheckersBoard,size);
     printf("Whites begin !\n");
 
-    do
-    {
-
-        do
-        {
-            do
-            {
+    do {
+        
+       
+        
+        do {
+            do {
                 printf("Enter the piece you want to move\n");
                 char input[2];
-                scanf("%s", input);
-                startcol = toupper(input[0]) - 'A';
-                startrow = size - (input[1] - '0');
-
+                scanf("%s",input);
+                startcol = toupper(input[0])-'A';
+                startrow = size-(input[1]-'0');
+                
             } while (startcol < 0 || startcol >= size || startrow < 0 || startrow >= size);
-            do
-            {
+            do {
                 printf("Where do you want to move this piece ?\n");
                 char input[2];
-                scanf("%s", input);
-                targcol = toupper(input[0]) - 'A';
-                targrow = size - (input[1] - '0');
+                scanf("%s",input);
+                targcol = toupper(input[0])-'A';
+                targrow = size-(input[1]-'0');
             } while (targcol < 0 || targcol >= size || targrow < 0 || targrow >= size);
-            if (CheckersBoard[startcol][startrow].color != turn)
-            {
-                printf("You can't move this piece !\n");
+            if (CheckersBoard[startcol][startrow].color != turn) {
+                    printf("You can't move this piece !\n");
             }
-        } while (!MoveCheck(CheckersBoard, size, startcol, startrow, targcol, targrow) || turn != CheckersBoard[startcol][startrow].color);
-
+        } while (!MoveCheck(CheckersBoard,size,startcol,startrow,targcol,targrow) || turn != CheckersBoard[startcol][startrow].color);
+        
         moves++;
-        turn = moves % 2;
-
-        if (turn == 0)
-        {
-            whitepieces = MoveExecution(CheckersBoard, startcol, startrow, targcol, targrow);
-            PrintBoard(CheckersBoard, size);
+        turn = moves%2;
+        
+        if (turn == 0) {
+            whitepieces = MoveExecution(CheckersBoard,startcol,startrow,targcol,targrow);
+            PrintBoard(CheckersBoard,size);
             printf("It's whites turn !\n");
-        }
-        else
-        {
-            blackpieces = MoveExecution(CheckersBoard, startcol, startrow, targcol, targrow);
-            PrintBoard(CheckersBoard, size);
+        } else {
+            blackpieces = MoveExecution(CheckersBoard,startcol,startrow,targcol,targrow);
+            PrintBoard(CheckersBoard,size);
             printf("It's blacks turn !\n");
         }
+        
+        
+    } while(blackpieces < size*3/2 && whitepieces < size*3/2);
 
-    } while (blackpieces < size * 3 / 2 && whitepieces < size * 3 / 2);
-
-    if (blackpieces == 0)
-    {
+    if (blackpieces == 0) {
         printf("Whites win !");
-    }
-    else
-    {
+    } else {
         printf("Blacks win !");
     }
+
 }
+
